c391aa113bec707672bdef60967ca8662c1ddb0c
Dan Siegel and Matt Botz <botzmatt@yahoo.com>
2016-06-22 20:54:01 -0700

    adds code to scripts.js to get us pointed in the right direction to fully get the function working

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
-var vowels = ["a", "e", "i", "o", "u", "y"]
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
-var input = "apple"
 var piglatin = function(wordinput) {
-  var firstLet = wordinput.charAt(0)
-for (var i=0; i <= vowels.length; i += 1); {
-    var letter = vowels[i];
-    if (letter === vowels) {
-      return (input + "ay");
-
+  var array = wordinput.split('');
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
+  var vowels = ["a", "e", "i", "o", "u", "y"]
+  var concatonatedword = '';
+  for (var i=0; i < vowels.length-1; i++); {
+    for(var h=0; h < vowels.wordinput-1; h++);
+      if(wordinput[h] === vowels[i]) {
+      console.log("apple" + "ay")
     }
   }
 };
 
-console.log(input);
+console.log(piglatin("apple"));
 
 
 // $(document).ready(function() {
